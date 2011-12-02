36cd43e1c81e4cfadee12873523fc4e3bf407f91
srinivas pandruvada <srinivas.pandruvada@intel.com>
2011-12-02 15:09:09 -0800

    x86 build config
    Added config.h to able to build for x86 target.
    
    Change-Id: I1ad50426a19614d7c3b62a43bc2f06ba0e08410f
    Signed-off-by: srinivas pandruvada <srinivas.pandruvada@intel.com>

---
+++ include/pci/config.h
@@ -0,0 +1,12 @@
+#define PCI_CONFIG_H
+#define PCI_OS_LINUX
+#define PCI_HAVE_PM_LINUX_SYSFS
+#define PCI_HAVE_PM_LINUX_PROC
+#define PCI_HAVE_LINUX_BYTEORDER_H
+#define PCI_PATH_PROC_BUS_PCI "/proc/bus/pci"
+#define PCI_PATH_SYS_BUS_PCI "/sys/bus/pci"
+#define PCI_HAVE_PM_DUMP
+#define PCI_IDS "pci.ids.gz"
+#define PCI_PATH_IDS_DIR "/usr/local/share"
+#define PCI_ID_DOMAIN "pci.id.ucw.cz"
+#define PCILIB_VERSION "3.1.8"
